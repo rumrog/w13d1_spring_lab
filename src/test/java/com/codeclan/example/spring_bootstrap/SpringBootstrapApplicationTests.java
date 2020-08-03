dd5d65ebdccc9b980b9dcbfaed2838ca3088aaa7
Rumen Roger <rumenroger@gmail.com>
2020-08-03 14:29:20 +0100

    End of Lab

--- src/test/java/com/codeclan/example/spring_bootstrap/SpringBootstrapApplicationTests.java
+++ src/test/java/com/codeclan/example/spring_bootstrap/SpringBootstrapApplicationTests.java
@@ -1,13 +1,25 @@
 package com.codeclan.example.spring_bootstrap;
 
+import com.codeclan.example.spring_bootstrap.models.Employee;
+import com.codeclan.example.spring_bootstrap.repositories.EmployeeRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootstrapApplicationTests {
 
+  @Autowired
+  EmployeeRepository employeeRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void createEmployee(){
+		Employee harrison = new Employee("Harrison", 22, 1234, "harrison@codeclan.com");
+		employeeRepository.save(harrison);
 	}
 
 }
