34ce2d34bfb56274775db478c5718dc17524262e
Rumen Roger <rumenroger@gmail.com>
2020-08-03 13:33:07 +0100

    Initial commit

---
+++ src/main/java/com/codeclan/example/spring_bootstrap/SpringBootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.spring_bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootstrapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/spring_bootstrap/SpringBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.spring_bootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
